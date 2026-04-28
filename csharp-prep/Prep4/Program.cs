d49ca4b51aa41a5d441c58ef4ba95c490f1c1a4b
John <60108868+jhjln87@users.noreply.github.com>
2026-04-27 20:22:25 -0600

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> nums = new List<int>();
+        int now = 1;
+        Console.WriteLine("\n\nEnter a list of numbers, type 0 when finished.");
+        while (now != 0) {
+            Console.Write("Enter number: ");
+            now = int.Parse(Console.ReadLine());
+            nums.Add(now);
+        }
+        float tot = 0, max = 0, sml = 9999;
+        for (int i = 0; i < nums.Count; i++) {
+            tot += nums[i];
+            if (nums[i] > max)
+            {
+                max = nums[i];
+            }
+            if (nums[i] < sml && nums[i] > 0) {
+                sml = nums[i];
+            }
+
+        }
+        Console.WriteLine($"The sum is {tot}\nThe average is: {tot/(nums.Count-1)}\nThe largest number is: {max}\nThe smallest positive number is: {sml}\nThe sorted list is:");
+        nums.Sort();
+        for (int i = 0; i < nums.Count; i++) {
+            Console.WriteLine(nums[i]);
+        }
     }
 }
